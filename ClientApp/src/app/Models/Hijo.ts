3852368dfc32edd20386a9e5b35bd1ccfa20638d
Sebastian Cáceres <sebastiancaceres207@gmail.com>
2024-04-26 12:31:41 -0500

    Agregando carpeta modelos

---
+++ ClientApp/src/app/Models/Hijo.ts
@@ -0,0 +1,12 @@
+export interface Personal{
+    IdHijo: number,
+    IdPersonal: number,
+    IdTipoDoc: number,
+    NumeroDoc: string,
+    ApPaterno: string,
+    ApMaterno: string,
+    Nombre1: string,
+    Nombre2: string,
+    NombreCompleto: string,
+    FechaNac: string,
+}
