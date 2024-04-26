70f4d3d42fbd74a4158bc0f79c042a05bf5d5c9e
Sebastian Cáceres <sebastiancaceres207@gmail.com>
2024-04-26 14:54:41 -0500

    Finalización de la página de inicio

---
+++ ClientApp/src/app/Pages/hijos/hijos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hijos',
+  standalone: true,
+  imports: [],
+  templateUrl: './hijos.component.html',
+  styleUrl: './hijos.component.css'
+})
+export class HijosComponent {
+
+}
--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { TrabajadoresComponent } from './Pages/trabajadores/trabajadores.component';
+import { HijosComponent } from './Pages/hijos/hijos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:InicioComponent},
+    {path: 'inicio', component:InicioComponent},
+    {path: 'Personal', component:TrabajadoresComponent},
+    {path: 'Hijos/Lista/:id', component:HijosComponent},
+];
